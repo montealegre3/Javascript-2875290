bbc597b5e7021c9aaa0f52549fd48508ff09cd09
montealegre3 <julianandrea.silva@gmail.com>
2024-07-19 14:56:16 -0500

    ejercicio

--- 03-dinamic-dom/app.js
+++ 03-dinamic-dom/app.js
@@ -1,6 +1,8 @@
 const listaColores = document.querySelector('#listaColores');
 const btnCrear = document.querySelector('#btnCrear');
 const main = document.querySelector('#main'); 
+const btnBorrar = document.querySelector('#btnBorrar');
+let n = 1; 
 
 function crear (){
     
@@ -8,12 +10,24 @@ function crear (){
     ball.innerText = "pelota";
     ball.classList.add('pelota');
     ball.style.backgroundColor = listaColores.value;
+    ball.innerText = n++;
 
     main.appendChild(ball);
-
-
     console.log(ball)
 
 }
 
-btnCrear.addEventListener('click', crear)
+function borrarPantalla (){
+    main.innerHTML = '';
+    n = 1;
+}
+function borrarElemento (e){
+    console.log('Le di click a la pelota: ' + e.target.innerText)
+    main.removeChild(e.target);
+}
+
+
+
+btnCrear.addEventListener('click', crear);
+btnBorrar.addEventListener('click', borrarPantalla);
+main.addEventListener('click', borrarElemento);
