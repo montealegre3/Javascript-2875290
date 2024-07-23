7274a6ee05b07601ffb158ea20a7f0febf243c40
montealegre3 <julianandrea.silva@gmail.com>
2024-07-23 16:03:43 -0500

    ejercicio

--- 4-lista-de-tareas/app.js
+++ 4-lista-de-tareas/app.js
@@ -1,5 +1,52 @@
 const inputBox = document.getElementById('input-box');
 const listContainer = document.getElementById('list-container');
 
-document.getElementById("add-btn").addEventListener("click", Nuevatarea)
+document.getElementById("add-btn").addEventListener("click", Nuevatarea);
+addEventListener("keydown", (e) => {
+    if(e.key == "Enter"){
+        Nuevatarea()
+    }
+})
+
+function Nuevatarea(){
+    if(inputBox.value == ""){
+        alert("Crea tu tarea")
+    }else{
+        let li= document.createElement("li");
+        li.innerHTML= inputBox.value;
+        let span= document.createElement("span");
+        span.innerHTML= "<i class='fa-solid fa-trash'></i>";
+        li.appendChild(span);
+        listContainer.appendChild(li);
+    }
+    inputBox.value = "";
+    SVGAnimatedPreserveAspectRatio()
+}
+
+// guardar datos en localLStorage
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML)
+}
+
+
+listContainer.addEventListener("click", (e) => {
+    if(e.target.tagName.toUpperCase() === "LI"){
+        e.target.classList.toggle("checked")
+        saveData()
+    }else if(e.target.tagName.toUpperCase() === "SPAN"){
+        e.target.parentElement.remove()
+        saveData()
+    }
+})
+
+// Recuperar datos de almacenamiento local 
+
+function showTask(){
+    listContainer.innerHTML= localStorage.getItem("data")
+}
+
+showTask()
+
+
 
