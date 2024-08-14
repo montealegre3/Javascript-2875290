f9204a885788caa43f82f3e7902c5b5d9703cf70
montealegre3 <julianandrea.silva@gmail.com>
2024-08-14 15:59:38 -0500

    api

--- 08-fetch-api/rickmorty.js
+++ 08-fetch-api/rickmorty.js
@@ -7,11 +7,56 @@ let results;
 
 
 function pintar (elemento){
-
+    switch (elemento) {
+        case 'Alive':
+          return 'green'
+          break
+        case 'Dead':
+          return 'red'
+          break
+        default:
+          return 'gray'
+      }
 }
 
-function filtrar(word, status){
-}
+function filtrar(e) {
+
+    e.preventDefault()
+  
+    container.innerHTML = ""
+  
+    const filtro = results.filter( word => word.status == listaStatus.value);
+  
+    console.log(filtro)
+  
+    filtro.forEach(element => {
+  
+      const card = document.createElement('div')
+      card.classList.add("card");
+      container.appendChild(card)
+  
+      card.innerHTML = ` 
+            <img src="${element.image}" width= "200px">
+            <div class="info">
+            <h1>${element.name}</h1>
+            <div class="section">
+            
+            <div class="circle ${pintar(element.status)}"></div>
+            <span class="estado">${element.status} - ${element.species} </span>
+            </div>
+            <div class="detalle">
+                <p><strong>Última ubicación conocida:</strong><br>${element.location.name}</p>
+                <p><strong>Visto por primera vez en:</strong><br>${element.origin.name}</p>
+            </div>
+        </div>
+        </div>
+        `
+      container.appendChild(card)
+    })
+  }
+  
+  buscador.addEventListener('submit',filtrar)
+  
 
 
 let url = "https://rickandmortyapi.com/api/character"
@@ -19,10 +64,10 @@ fetch(url)
 .then(response => response.json())
 .then(data => {
 
+    results = data.results;
+
     /* const result = data.result.filter( word => word.status == listaStatus.value);
-    console.log(result) */
-
-
+    console.log(result)
 
     data.results.forEach(element => {
         console.log(data.results)
@@ -46,8 +91,11 @@ fetch(url)
         </div>
         `
         container.appendChild(card)
-    });
+    }); */
 }
 )
+
+
+
 
 
