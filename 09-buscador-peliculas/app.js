9d81e150646566720e874324e94b8f7973a67177
montealegre3 <julianandrea.silva@gmail.com>
2024-08-15 15:54:20 -0500

    series

---
+++ 09-buscador-peliculas/app.js
@@ -0,0 +1,25 @@
+const container = document.querySelector('.container')
+
+seriesAnimadas.forEach( element => {
+    let card = document.createElement('div');
+
+    card.innerHTML = `
+    <div class="container">
+        <div class="card1">
+            <img src="${element.imagen}" alt="animaniacs(serie)">
+            <div class="intro">
+                <h1>${element.nombre}</h1>
+                <p>Año: ${element.year}</p>
+                <p>Género: ${element.genero}</p>
+                <p>Creador: ${element.creador}</p>
+                <p>Temporadas: ${element.numeroDetemporadas}</p>
+                <p>Episodios: ${element.numeroDeEpisodios}</p>
+            </div>
+        </div>
+    </div> 
+    `
+
+    container.appendChild(card);
+})
+
+
